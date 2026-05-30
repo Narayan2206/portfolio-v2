3b99523afaa82c7f5d5398ae4677b79005694959
Narayan Pal <narayan220602@gmail.com>
2026-05-30 17:34:25 +0530

    add projects

---
+++ src/data/projects.ts
@@ -0,0 +1,9 @@
+export const projects = [
+    {
+        title: "Slate - Workspace & Documents",
+        description: "",
+        tech: ["TypeScript", "JavaScript", "Next.js", "Supabase", "TipTap", "TailwindCSS"],
+        github: "https://github.com/Narayan2206/knowledge-workspace",
+        demo: "https://knowledge-workspace-three.vercel.app/",
+    }
+]
